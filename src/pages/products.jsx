4ccd746beb1a654620869c5ae007a2e36a3e30fa
codeCamper07 <dommaraju.bhargav007@gmail.com>
2023-12-26 12:58:01 +0530

    newww

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Layout from '../components/layout'
 import ProductList from '../components/productsList'
-import { TailSpin } from 'react-loader-spinner'
 import '../styles/products.css'
 import Cookies from 'js-cookie'
 
@@ -26,27 +25,15 @@ const Products = () => {
     return () => getData()
   }, [productList])
 
-  const loadingScreen = () => {
-    return (
-      <div className='loader-container'>
-        <TailSpin color='#00BFFF' height={100} width={100} />
-      </div>
-    )
-  }
-
   return (
     <Layout>
       <div className='product-container'>
         <h1>products</h1>
-        {productList.length === 0 ? (
-          loadingScreen()
-        ) : (
-          <div className='card-container'>
-            {productList.map((item) => {
-              return <ProductList prods={item} key={item.id} />
-            })}
-          </div>
-        )}
+        <div className='card-container'>
+          {productList.map((item) => {
+            return <ProductList prods={item} key={item.id} />
+          })}
+        </div>
       </div>
     </Layout>
   )
