8243cd926dde4d1b2116254f332765c73499a1a9
codeCamper07 <dommaraju.bhargav007@gmail.com>
2023-12-26 23:32:54 +0530

    mathErrorFIXED

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -71,10 +71,7 @@ function Cart() {
                   <td colspan='4'>Total</td>
                   <td>
                     {cartItems
-                      .reduce(
-                        (total, item) => total + item.price * item.quantity,
-                        0,
-                      )
+                      .reduce((total, item) => total + item.price, 0)
                       .toFixed(2)}
                   </td>
                 </tr>
